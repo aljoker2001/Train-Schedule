24848398be26ae420272d0f7336d145e8e0f1975
aljoker2001 <aljoker2001@hotmail.com>
2019-04-05 22:09:33 -0700

    html and css are mostly complete.  Ready to begin js

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+var config = {
+    apiKey: "AIzaSyA985y_KrL_WgBYxsu1W0GoWN40jC9mFe4",
+    authDomain: "train-schedule-da878.firebaseapp.com",
+    databaseURL: "https://train-schedule-da878.firebaseio.com",
+    projectId: "train-schedule-da878",
+    storageBucket: "train-schedule-da878.appspot.com",
+    messagingSenderId: "280977608178"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+  
